e337d824fee0bb0825191d21352eed2465305fd6
Vikas <vikas.kumar.singh1198@gmail.com>
2024-12-28 14:05:34 +0530

    small edit

--- src/components/SecondContainer.jsx
+++ src/components/SecondContainer.jsx
@@ -1,25 +1,21 @@
-import { useSelector } from "react-redux"
-import MovieList from "./MovieList"
+import { useSelector } from "react-redux";
+import MovieList from "./MovieList";
 
 const SecondContainer = () => {
+  const movies = useSelector((store) => store.movies?.nowPlayingMovies);
 
-  const movies = useSelector(store=>store.movies?.nowPlayingMovies);
+  return (
+    movies && (
+      <div>
+        <div className="-mt-60">
+          <MovieList title={"Now Playing"} movies={movies}></MovieList>
+          <MovieList title={"Popular"} movies={movies}></MovieList>
+          <MovieList title={"Trending"} movies={movies}></MovieList>
+          <MovieList title={"Horror"} movies={movies}></MovieList>
+        </div>
+      </div>
+    )
+  );
+};
 
-
-  return ( movies &&
-    <div>
-      
-        
-          <div className="-mt-60">
-            <MovieList title={"Now Playing"} movies={movies} ></MovieList>
-            <MovieList title={"Popular"} movies={movies} ></MovieList>
-            <MovieList title={"Trending"} movies={movies} ></MovieList>
-            <MovieList title={"Horror"} movies={movies} ></MovieList>
-          </div>
-        
-       
-    </div>
-  )
-}
-
-export default SecondContainer
+export default SecondContainer;
